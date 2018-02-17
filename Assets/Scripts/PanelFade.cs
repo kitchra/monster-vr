03f85da94c3c43a46dc1fedf4fd8e833a5443d02
rakitch <rachelk4@vt.edu>
2018-02-17 16:02:35 -0500

    Changes to Opening Screen UI
    Particles, design changes, scripts, etc.

---
+++ Assets/Scripts/PanelFade.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PanelFade : MonoBehaviour
+{
+
+    public Material m;
+    public float _colorSpeed = 0.01f;
+    private Color c;
+    private bool runFadeOut = false;
+    private bool runFadeIn = false;
+    private string levelName = "";
+
+    // Use this for initialization
+    void Start()
+    {
+        FadeOut();
+    }
+
+    void Update()
+    {
+        if (runFadeIn)
+        {
+            if (c.a < 1.0f)
+            {
+                c.a = c.a + _colorSpeed;
+            }
+            else
+            {
+                runFadeIn = false;
+                if (levelName != "")
+                {
+                    SceneManager.LoadScene(levelName);
+                }
+            }
+
+            m.color = c;
+        }
+        if (runFadeOut)
+        {
+            if (c.a > 0.0f)
+            {
+                c.a = c.a - _colorSpeed;
+            }
+            else
+            {
+                runFadeOut = false;
+            }
+
+            m.color = c;
+        }
+    }
+
+    public void FadeIn()
+    {
+        c = m.color;
+        c.a = 0.0f;
+        runFadeIn = true;
+    }
+
+    public void FadeOut()
+    {
+        c = m.color;
+        c.a = 1.0f;
+        runFadeOut = true;
+    }
+
+    public void loadLevel(string _levelName)
+    {
+        levelName = _levelName;
+        FadeIn();
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void loadScene(string sceneName)
+    {
+        PanelFade pf = GameObject.Find("PanelFadeObject").GetComponent<PanelFade>();
+        pf.loadLevel(sceneName);
+    }
+}
