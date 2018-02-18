301e9ea4e6e082f157d59e9b82cbc9fe3467e9d4
U-DESKTOP-NK9HBUP\Kenneth Worden <kenny.worden@gmail.com>
2018-02-17 17:36:33 -0800

    Make text better resolution

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+
+    private float moveSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+	}
+}
